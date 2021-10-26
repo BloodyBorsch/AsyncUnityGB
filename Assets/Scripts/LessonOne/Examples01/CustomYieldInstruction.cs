d53a9c84f212997fff575ceb18ea2e8fcc88e517
Maks <56753588+BloodyBorsch@users.noreply.github.com>
2021-10-26 22:28:49 +0300

    Lesson Two beginning

---
+++ Assets/Scripts/LessonOne/Examples01/CustomYieldInstruction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+public abstract class CustomYieldInstruction : IEnumerator
+{
+    public abstract bool keepWaiting
+    {
+        get;
+    }
+
+    public object Current => null;
+
+    public abstract bool MoveNext();
+
+    public abstract void Reset();
+}
---
+++ Assets/Scripts/LessonOne/Examples01/WaitForMouseDown.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class WaitForMouseDown : CustomYieldInstruction
+{
+    public override bool keepWaiting => !Input.GetMouseButtonDown(1);
+
+    public override bool MoveNext()
+    {
+        return keepWaiting;
+    }
+
+    public override void Reset()
+    {
+    }
+
+    public WaitForMouseDown()
+    {
+        Debug.Log("Waiting for Mouse right button down");
+    }
+}

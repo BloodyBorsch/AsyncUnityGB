56fd7b49b0203c2a14204bc913d337af05a0e9fe
Maks <56753588+BloodyBorsch@users.noreply.github.com>
2021-10-28 13:48:31 +0300

    ObjectiveOneDone

---
+++ Assets/Scripts/LessonTwo/RecalculateJob.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+
+namespace LessonTwo
+{
+    public struct RecalculateJob : IJobParallelFor
+    {        
+        public NativeArray<int> RecalculateNumbers;
+
+        public void Execute(int index)
+        {
+            if (RecalculateNumbers[index] > 10)
+            {
+                RecalculateNumbers[index] = 0;                
+            }
+        }
+    }
+}

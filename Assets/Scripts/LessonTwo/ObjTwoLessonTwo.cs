13fb9433982b4e9c73f3d7bd93b23009811b6e15
Maks <56753588+BloodyBorsch@users.noreply.github.com>
2021-10-28 14:50:51 +0300

    Objective two is done

---
+++ Assets/Scripts/LessonTwo/ObjTwoLessonTwo.cs
@@ -0,0 +1,66 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+
+namespace LessonTwo
+{
+    public class ObjTwoLessonTwo : MonoBehaviour
+    {
+        private int _numberOfentities = 50;
+        private int _batchCount = 5;
+        private int _startDistance = 5;
+        private int _startVelocity = 2;
+
+        private NativeArray<Vector3> _positions;
+        private NativeArray<Vector3> _velocities;
+        private NativeArray<Vector3> _finalPositions;
+        private PositionsAndVelocities _startPositions;
+        private JobHandle _handler;
+
+        private void Start()
+        {
+            _positions = new NativeArray<Vector3>(_numberOfentities, Allocator.Persistent);
+            _velocities = new NativeArray<Vector3>(_numberOfentities, Allocator.Persistent);
+            _finalPositions = new NativeArray<Vector3>(_numberOfentities, Allocator.Persistent);
+            _startPositions = new PositionsAndVelocities();
+
+            if (Calculation())
+            {
+                _handler = _startPositions.Schedule(_numberOfentities, _batchCount);
+                _handler.Complete();
+                if (_handler.IsCompleted) ShowResult();
+            }            
+        }
+
+        private bool Calculation()
+        {
+            _startPositions.Positions = _positions;
+            _startPositions.Velocities = _velocities;
+
+            for (int i = 0; i < _numberOfentities; i++)
+            {
+                _startPositions.Positions[i] = Random.insideUnitSphere * Random.Range(0, _startDistance);
+                _startPositions.Velocities[i] = Random.insideUnitSphere * Random.Range(0, _startVelocity);
+            }
+
+            return true;
+        }
+
+        private void ShowResult()
+        {
+            for (int i = 0; i < _numberOfentities; i++)
+            {
+                _finalPositions[i] = _startPositions.Positions[i] + _startPositions.Velocities[i];
+                Debug.Log($"Final Position {i + 1} is {_finalPositions[i]}");
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _finalPositions.Dispose();
+            _positions.Dispose();
+            _velocities.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/LessonTwo/PositionsAndVelocities.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+
+namespace LessonTwo
+{
+    public struct PositionsAndVelocities : IJobParallelFor
+    {
+        public NativeArray<Vector3> Positions;
+        public NativeArray<Vector3> Velocities;
+
+        public void Execute(int index)
+        {
+            Positions[index] = Positions[index].normalized;
+            Velocities[index] = Velocities[index].normalized;
+        }
+    }
+}
